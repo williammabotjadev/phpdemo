2b32f209276316014f4b79cb4f613eea220a8c1b
williammabotjadev <williammabotjadev@gmail.com>
2022-11-04 23:22:59 +0200

    table-success, table-hover, table element

--- index.php
+++ index.php
@@ -10,25 +10,25 @@
     <title>EdinEstates | A Modern Real Estate Solution</title>
 </head>
 <body>
-    <div style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+    <div class="table-responsive" style="display: flex; flex-direction: column; width: 100%; justify-content: center; align-items: center">
     <br />
     <h3>Welcome to EdinEstates API</h3>
     <h6> A modern real estate solution</h6>
     <?php 
         $count = 0;
         $file = fopen('./static/dummy_data.csv', 'r');
-        echo "<html><body><center><table border='1'>\n\n";
+        echo "<html><body><center><table class='table table-hover table-success' border='1'>\n\n";
         echo "<br />";
         while(($row = fgetcsv($file, 1000, ',')) !== false)
         {
             echo "<tr>";
             foreach($row as $item) 
             {   
-                if ($count > 0)
+                if ($count > 7)
                 {
                     echo "<td>".$item."</td>";
                 } else {
-                    echo "<th>".$item."</th>";
+                    echo "<th class='col'>".$item."</th>";
                     $count++;
                 }
                
